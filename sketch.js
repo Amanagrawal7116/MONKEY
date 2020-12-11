f808d07aad1d71614ad7db0e23167b041afa1870
Amanagrawal7116 <74959306+Amanagrawal7116@users.noreply.github.com>
2020-12-11 15:42:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,6 +74,7 @@ function draw() {
   
     if((touches.length >0 ||keyDown("space") ) &&player.y >= height-120) {
       player.velocityY = -12;
+      touches = [];
       }
     player.velocityY = player.velocityY + 0.8;
   
